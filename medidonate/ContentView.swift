dfafef803d5118d379ab3144382f40a3a3750687
kassisfares <fareskas123@gmail.com>
2024-03-07 16:57:40 +0100

    sign in page (testing)

--- medidonate/ContentView.swift
+++ medidonate/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Circle()
+                .fill(Color.primarycolor)
+                .frame(width: 1000, height: 575)
+                .offset(x: -140, y: -480)
+                .rotationEffect(.degrees(10))
+            Text("Login Now!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .offset(x: -60, y: -290)
+            Image(systemName: "arrowshape.left.circle")
+                .font(.system(size: 30))
+                .foregroundColor(Color.white)
+                .frame(width: 200, height: 200)
+                .offset(x: -160, y: -360)
         }
-        .padding()
     }
 }
 
