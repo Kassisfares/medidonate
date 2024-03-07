e7b3e36aac2541486989c113cd636983beec5de0
kassisfares <fareskas123@gmail.com>
2024-03-07 16:37:05 +0100

    Initial Commit

---
+++ medidonate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  medidonate
+//
+//  Created by fares kassis on 7/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
